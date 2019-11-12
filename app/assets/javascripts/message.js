f305b7745ce55b6fa72ac5539555ddafead35e50
moripp <msa.m16a.i212z4a@gmail.com>
2019-11-12 20:48:00 +0900

    Add ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -5,5 +5,13 @@ $(function() {
     let url = location.href
     let formData = new FormData($(".new_message").get(0));
 
+    $.ajax({
+      url: (url),
+      type: "POST",
+      data: (formData),
+      dataType: "json",
+      processData: false,
+      contentType: false
+    });
   });
 });
