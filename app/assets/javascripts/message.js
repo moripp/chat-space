a439d3489e8dbac5cfba345557718b51f78f9bcb
moripp <msa.m16a.i212z4a@gmail.com>
2019-11-12 20:47:41 +0900

    get URL and FormData

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,9 @@
 $(function() {
   $(".new_message").on('submit', function(e) {
     e.preventDefault();
+
+    let url = location.href
+    let formData = new FormData($(".new_message").get(0));
+
   });
 });
