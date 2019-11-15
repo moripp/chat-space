b7f2d6c047429cef3df40bd3fc24de3a6dc1476c
moripp <msa.m16a.i212z4a@gmail.com>
2019-11-15 17:42:11 +0900

    Create HTML
    返ってきたJSONからHTML作成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,25 @@
 $(function() {
+  function buildHTML(message) {   
+    var html_content = message.content==null ? "" : `<p class="wrapper__maine__contents__message__text__content">${message.content}</p>`;
+    var html_image = message.image==null ? "" : `<img class="wrapper__maine__contents__message__text__image" src="${message.image}">`;
+    var date = message.created_at
+    var html = `<div class="wrapper__maine__contents__message">
+                  <div class="wrapper__maine__contents__message__info">
+                    <div class="wrapper__maine__contents__message__info__user">
+                      ${message.user_name}
+                    </div>
+                    <div class="wrapper__maine__contents__message__info__date">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="wrapper__maine__contents__message__text">
+                    ${html_content}
+                    ${html_image}
+                  </div>
+                </div>`
+    return html;
+  }
+
   $(".new_message").on('submit', function(e) {
     e.preventDefault();
 
